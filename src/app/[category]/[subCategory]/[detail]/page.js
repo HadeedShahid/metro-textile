5051b80f1ea6c6038e5ed8acf0b559007adaf726
HadeedShahid <Hadeed.shahid08@gmail.com>
2025-10-04 19:43:26 +0500

    added specification grid

--- src/app/[category]/[subCategory]/[detail]/page.js
+++ src/app/[category]/[subCategory]/[detail]/page.js
@@ -1,5 +1,10 @@
 import Breadcrumbs from "@/components/common/breadcrumbs";
+import Call from "@/components/common/call";
+import Email from "@/components/common/email";
 import GalleryGrid from "@/components/common/imageGallery";
+import SpecificationGrid from "@/components/common/specificationGrid";
+import Whatsapp from "@/components/common/whatsapp";
+import { Card } from "@/components/ui/card";
 import React from "react";
 
 const galleryImages = [
@@ -27,7 +32,7 @@ const galleryImages = [
 
 const Detail = () => {
   return (
-    <section>
+    <section clas>
       <Breadcrumbs
         items={[
           { label: "Home", href: "/" },
@@ -37,6 +42,19 @@ const Detail = () => {
       />
       <h1 className="font-semibold text-4xl">Yellow Metal Button</h1>
       <GalleryGrid />
+      <div className="grid grid-cols-3 gap-2">
+        <div className="col-span-2 pr-12">
+          <SpecificationGrid />
+        </div>
+        <div className="col-span-1">
+          <Card className={"px-4"}>
+            <h3>Contact</h3>
+            <Call />
+            <Email />
+            <Whatsapp />
+          </Card>
+        </div>
+      </div>
     </section>
   );
 };
